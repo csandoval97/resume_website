b3c5b44665af0bd1d8cca4b3ca8541599bb86e22
csando6 <csando6@uic.edu>
2019-10-20 18:43:52 -0500

    my website

---
+++ resume-website/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ResumeComponent } from './component/resume/resume.component';
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent },
+  {path:'resume', component:ResumeComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ resume-website/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { ResumeComponent } from './component/resume/resume.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ResumeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PdfViewerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
